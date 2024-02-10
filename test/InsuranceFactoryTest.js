a52cf3811d9cea5d312af4aa12199bebb98d2883
pras7777 <150328549+pras7777@users.noreply.github.com>
2024-02-10 13:01:59 +0530

    Update InsuranceFactoryTest.js

--- test/InsuranceFactoryTest.js
+++ test/InsuranceFactoryTest.js
@@ -33,31 +33,29 @@ describe("InsuranceFactory", () => {
         expect(contractAddress).to.not.be.null;
     });
 
-    // Test case to check correct insurance contract retrieval for an owner
+  
     it("should return the correct insurance contract for an owner", async () => {
-        // Retrieving contract addresses for two different owners
+    
         const contractAddress1 = await insuranceFactory.getInsuranceContractByOwner(owner.address);
         const contractAddress2 = await insuranceFactory.getInsuranceContractByOwner(addr1.address);
 
-        // Expecting these addresses to be different, signifying unique contracts for each owner
+     
         expect(contractAddress1).to.not.equal(contractAddress2);
     });
 
-    // Test case to ensure the list of deployed insurance contracts is correct
+ 
     it("should return the list of deployed insurance contracts", async () => {
-        // Creating another insurance contract with a different account
+     
         await insuranceFactory.connect(addr2).createInsurance();
 
-        // Retrieving the list of all deployed insurance contracts
+      
         const deployedContracts = await insuranceFactory.getDeployedInsuranceContracts();
 
-        // Expecting the length of deployed contracts array to be 2 (two contracts created)
         expect(deployedContracts).to.have.lengthOf(2);
     });
-
-    // Test case to check prevention of multiple contracts for the same address
+s
     it("should not allow creating multiple contracts for the same address", async () => {
-        // Expecting an error when trying to create a second insurance contract for the same address
+       
         await expect(insuranceFactory.createInsurance()).to.be.revertedWith(
             "Contract already exists for this address"
         );
