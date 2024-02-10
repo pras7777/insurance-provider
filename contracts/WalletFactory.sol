3e012df57f5eb4941aad9bcb773bc57aa8307b35
pras7777 <150328549+pras7777@users.noreply.github.com>
2024-02-10 12:58:37 +0530

    Update WalletFactory.sol

--- contracts/WalletFactory.sol
+++ contracts/WalletFactory.sol
@@ -3,29 +3,20 @@ pragma solidity ^0.8.19;
 
 import "./Wallet.sol";
 
-/**
- * @title WalletFactory
- * @notice This factory contract enables users to create their own instance of the Wallet contract.
- *         It keeps track of each deployed contract and associates them with their respective owners.
- */
+
 contract WalletFactory {
     address payable public verifierCompany; // Address of the verifier company
     address[] public deployedContracts; // Stores addresses of all deployed Wallet contracts
     mapping(address => address) public contractOwners; // Maps user addresses to their Wallet contract addresses
     mapping(address => bool) public hasContract; // Tracks whether a user has deployed a Wallet contract
 
-    /**
-     * @dev Sets the verifier company's address upon deployment.
-     * @param _verifierCompanyAddress Address of the verifier company.
-     */
+ 
     constructor(address payable _verifierCompanyAddress) {
         require(_verifierCompanyAddress != address(0), "Verifier company address cannot be the zero address");
         verifierCompany = _verifierCompanyAddress;
     }
 
-    /**
-     * @notice Creates a new Wallet contract instance for the caller.
-     */
+
     function createWallet() external {
         require(
             !hasContract[msg.sender],
@@ -38,19 +29,11 @@ contract WalletFactory {
         hasContract[msg.sender] = true;
     }
 
-    /**
-     * @notice Retrieves the Wallet contract address for a specific owner.
-     * @param ownerAddress The address of the contract owner.
-     * @return The address of the corresponding Wallet contract.
-     */
+
     function getWalletByOwner(address ownerAddress) external view returns (address) {
         return contractOwners[ownerAddress];
     }
 
-    /**
-     * @notice Gets all deployed Wallet contract addresses.
-     * @return A list of addresses of all deployed Wallet contracts.
-     */
     function getAllDeployedWallets() external view returns (address[] memory) {
         return deployedContracts;
     }
